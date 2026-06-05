208c0098a3fe697642a8dec3b6d1b69fa9823be5
Bisma <bisma007bisma@gmail.com>
2026-06-05 21:55:50 +0500

    Added if else if control flow statements examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Counter } from './counter/counter';
 import { Getset } from './getset/getset';
+import { Ifelse } from './ifelse/ifelse';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Counter, Getset ],
+  imports: [RouterOutlet, Counter, Getset, Ifelse ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
