753e2ce19185d1ec3f7a6ce34298a89b55acf56f
Bisma <bisma007bisma@gmail.com>
2026-06-08 20:06:41 +0500

    practice update

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,14 @@ import { Counter } from './counter/counter';
 import { Getset } from './getset/getset';
 import { Ifelse } from './ifelse/ifelse';
 import { Switchcase } from './switchcase/switchcase';
+import { Forloop } from './forloop/forloop';
 
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Counter, Getset, Ifelse, Switchcase ],
+  imports: [RouterOutlet, Counter, Getset, Ifelse, Switchcase, Forloop ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/forloop/forloop.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forloop',
+  imports: [],
+  templateUrl: './forloop.html',
+  styleUrl: './forloop.css',
+})
+export class Forloop {
+  items = ['apple', 'mango', 'banana']
+}
